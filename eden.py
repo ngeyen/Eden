88a3fa332d80547dc5d1c0d947a84bad6f7ac5f0
ngeyen <ngeyennv@hotmail.com>
2020-08-01 16:40:10 +0200

    debuging Lexer class product

--- eden.py
+++ eden.py
@@ -9,9 +9,9 @@ DIGITS = '0123456789'
 class Error:
     def __init__(self, error_name, details):
         self.error_name = error_name
-        self.detais = @staticmethod
+        self.details = details
     def as_string(self):
-        result = f'{self.error_name}: {self.detais}'
+        result = f'{self.error_name}: {self.details}'
         return result
 
 class IllegalCharError(Error):
@@ -33,19 +33,19 @@ TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 
 class Token:
-    def __init__(self, type_, value):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
-        return f'{self.type}
+        return f'{self.type}'
 
 
 #LEXER
 
 class Lexer:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, text):
         self.text = text
         self.pos = -1 
         self.current_char = None
@@ -53,9 +53,9 @@ class Lexer:
 
     def advance(self):
         self.pos += 1
-        self.current_char = self.text[pos] if self.pos < len(self.text) else None
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
-    def make_tokens(self)
+    def make_tokens(self):
         tokens = []
 
         while self.current_char !=None:
@@ -92,22 +92,28 @@ class Lexer:
         return tokens, None
 
     def make_number(self):
-        num_str =''
+        num_str = ''
         dot_count = 0
 
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count == 1:
-                    num_str += '.'
-                else:
-                    num_str += self.current_char
+                if dot_count == 1: 
+                    break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
                 
-        if dot_count ==0:
-            return Token(TT_INT, INT(num_str))
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
 
+
+####################################
 # RUN
+####################################
 
 def run(text):
     lexer = Lexer(text)
--- shell.py
+++ shell.py
@@ -1,3 +1,10 @@
+import eden
+
 while True:
     text = input('eden > ')
+    result, error = eden.run(text)
+    if error: 
+        print(error.as_string())
+    else: 
+        print (result)
     
