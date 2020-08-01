90adb4bd7845fb3391c084cbc7932cbd1fadcb3f
ngeyen <ngeyennv@hotmail.com>
2020-08-01 22:32:33 +0200

    Created position class to keep tract of the line in which the error is has been caused

--- lang/eden.py
+++ lang/eden.py
@@ -1,22 +1,52 @@
 #######################
-#CONSTANTS
+# CONSTANTS
 #######################
 
 DIGITS = '0123456789'
 
 
-
+#####################
+# ERROR
+####################
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
         self.error_name = error_name
         self.details = details
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
+
 
 class IllegalCharError(Error):
     def __init__(self, details):
         super().__init__('Illegal Character', details)
+
+
+# POSITION
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col += 0
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 
 ########################
@@ -32,6 +62,7 @@ TT_DIV = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 
+
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
@@ -42,26 +73,27 @@ class Token:
         return f'{self.type}'
 
 
-#LEXER
+# LEXER
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1 
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance()
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
 
-        while self.current_char !=None:
+        while self.current_char is not None:
             if self.current_char in ' \t':
                 self.advance()
-            
+
             if self.current_char in DIGITS:
                 tokens.append(self.make_number())
 
@@ -83,39 +115,41 @@ class Lexer:
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
-            
+
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
-        
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+
         return tokens, None
 
     def make_number(self):
         num_str = ''
         dot_count = 0
 
-        while self.current_char != None and self.current_char in DIGITS + '.':
+        while self.current_char is not None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count == 1: 
+                if dot_count == 1:
                     break
                 dot_count += 1
                 num_str += '.'
             else:
                 num_str += self.current_char
             self.advance()
-                
+
         if dot_count == 0:
             return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
 
+
 ####################################
 # RUN
 ####################################
 
-def run(text):
-    lexer = Lexer(text)
+def run(text, fn):
+    lexer = Lexer(fn, text)
     token, error = lexer.make_tokens()
 
     return token, error
--- shell.py
+++ shell.py
@@ -1,10 +1,9 @@
-import eden
+from lang import eden
 
 while True:
     text = input('eden shell >>> ')
-    result, error = eden.run(text)
-    if error: 
+    result, error = eden.run(text, '<stdin>')
+    if error:
         print(error.as_string())
-    else: 
-        print (result)
-    
+    else:
+        print(result)
